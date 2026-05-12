8ccd3874a9e648ed6b7598f703783f5fee3ab13d
Reejal Choudhary <reejalree@gmail.com>
2026-05-12 11:57:56 +0530

    server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/api/health", (req, res) => {
 app.use("/api", apiRoutes);
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 API Pariksha backend running on http://localhost:${PORT}`)
 );
